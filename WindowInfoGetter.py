6bac1f037961e6dd240acfdb02cf0fa8bad3bdb4
Christoffer Nilsson <Coffe93p@gmail.com>
2023-05-13 01:43:39 +0200

    Added loading of application and window name for win32

--- WindowInfoGetter.py
+++ WindowInfoGetter.py
@@ -1,14 +1,7 @@
 # pylint: disable=no-name-in-module
 
 import sys
-
-from AppKit import NSWorkspace  # E0611: No name 'NSWorkspace' in module 'AppKit'
-
-from Quartz import (
-    CGWindowListCopyWindowInfo,  # E0611: No name 'CGWindowListCopyWindowInfo' in module 'Quartz'
-    kCGWindowListOptionOnScreenOnly,  # E0611: No name 'kCGWindowListOptionOnScreenOnly' in module 'Quartz'
-    kCGNullWindowID,  # E0611: No name 'kCGNullWindowID' in module 'Quartz'
-)
+import importlib.util
 
 
 class WindowInfoGetter:
@@ -26,11 +19,44 @@ class WindowInfoGetter:
 
     @staticmethod
     def _get_active_window_win32():
-        # TODO: Implement this function when I get code on my Windows machine
-        pass
+        spam_spec = importlib.util.find_spec("win32gui")
+        if spam_spec is None:
+            raise ModuleNotFoundError(
+                "You must have pywin32 installed to use this module"
+            )
+
+        import win32gui
+
+        window = win32gui.GetForegroundWindow()
+        window_name = win32gui.GetWindowText(window)
+        application_name = win32gui.GetClassName(window)
+
+        return WindowInfoGetter._review_active_info_win32(application_name, window_name)
 
     @staticmethod
     def _get_active_window_darwin():
+        spam_spec = importlib.util.find_spec("AppKit")
+        if spam_spec is None:
+            raise ModuleNotFoundError(
+                "You must have pyobjc installed to use this module"
+            )
+
+        spam_spec = importlib.util.find_spec("Quartz")
+        if spam_spec is None:
+            raise ModuleNotFoundError(
+                "You must have pyobjc installed to use this module"
+            )
+
+        from AppKit import (
+            NSWorkspace,
+        )  # E0611: No name 'NSWorkspace' in module 'AppKit'
+
+        from Quartz import (
+            CGWindowListCopyWindowInfo,  # E0611: No name 'CGWindowListCopyWindowInfo' in module 'Quartz'
+            kCGWindowListOptionOnScreenOnly,  # E0611: No name 'kCGWindowListOptionOnScreenOnly' in module 'Quartz'
+            kCGNullWindowID,  # E0611: No name 'kCGNullWindowID' in module 'Quartz'
+        )
+
         curr_pid = NSWorkspace.sharedWorkspace().activeApplication()[
             "NSApplicationProcessIdentifier"
         ]
@@ -46,10 +72,12 @@ class WindowInfoGetter:
                 #     f"DEBUG: {owner_name} - {window_name} (PID: {pid}, WID: {window_number}): {window_name}"
                 # )
 
-        return WindowInfoGetter._review_active_info(application_name, window_name)
+        return WindowInfoGetter._review_active_info_darwin(
+            application_name, window_name
+        )
 
     @staticmethod
-    def _review_active_info(application_name, window_name):
+    def _review_active_info_darwin(application_name, window_name):
         if application_name == "Safari":
             if " - Safari" in window_name:
                 window_name = window_name[: -len(" - Safari")]
@@ -61,6 +89,17 @@ class WindowInfoGetter:
         elif application_name == "iTerm2":
             window_name = "Terminal"
 
+        return application_name, window_name
+
+    @staticmethod
+    def _review_active_info_win32(application_name, window_name):
+        window_name = window_name.replace("-", "—")
+        window_name_sections = window_name.split("—")
+        application_name = window_name_sections.pop(
+            len(window_name_sections) - 1
+        ).strip()
+        window_name = window_name_sections.pop().strip()
+        print(f"Application name: {application_name} - Window name: {window_name}")
         return application_name, window_name
 
     @staticmethod
